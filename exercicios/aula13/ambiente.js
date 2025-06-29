e860cae9b5884d0c631a68af64eb9be4ecc4e2f5
Joaquim <joaquimlima0612@gmail.com>
2025-06-29 12:03:49 -0300

    Estrutura de Repetição

--- exercicios/aula13/ambiente.js
+++ exercicios/aula13/ambiente.js
@@ -1,9 +1,19 @@
 var c = 1
 
+/*
+
 while(c < 6) { // Enquanto o "c" for menor que 6 vezes (irá aparecer 5 passos pois o "c" não é menor OU IGUAL a 6)
     console.log(`Passo ${c}`)
     c++ // Adicionando 1 a cada loop
 }
+
+*/
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+}
+while (c < 6)
 
 /*
 console.log('Tudo bem?')
