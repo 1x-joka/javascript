eb10850b8475c672864aca6c442d5309ed17259d
Joaquim <joaquimlima0612@gmail.com>
2025-06-29 11:59:37 -0300

    Create ambiente.js

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,15 @@
+var c = 1
+
+while(c < 6) { // Enquanto o "c" for menor que 6 vezes (irá aparecer 5 passos pois o "c" não é menor OU IGUAL a 6)
+    console.log(`Passo ${c}`)
+    c++ // Adicionando 1 a cada loop
+}
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
