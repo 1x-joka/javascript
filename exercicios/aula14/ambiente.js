3a3d641e71d91f6f025839d73eea9ac7d2d99269
Joaquim <joaquimlima0612@gmail.com>
2025-06-29 15:04:56 -0300

    Estrutura For

---
+++ exercicios/aula14/ambiente.js
@@ -0,0 +1,16 @@
+/*
+var c = 1
+
+do {
+    c++
+    console.log(c)
+} while (c < 5)
+ */
+
+//-----------
+
+console.log('Vai começar...')
+for (var c = 1; c <= 5; c++) { // for (Inicialização; Teste Lógico; Incremento)
+    console.log(c)
+}
+console.log('FIM!')
