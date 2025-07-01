01ee168b39b4a0c4f2e8e64ad2c5e3ddb8e11412
Joaquim <joaquimlima0612@gmail.com>
2025-07-01 13:31:22 -0300

    Update ambiente.js

--- exercicios/aula15/ambiente.js
+++ exercicios/aula15/ambiente.js
@@ -5,3 +5,14 @@ num.push(1) // O 1 não ficou na ordem crescente pois esse comando já foi dado,
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+let pos = num.indexOf(8) // Buscando qual a chave do conteúdo 8
+console.log(`O valor 8 está na posição ${pos}`) // Ele está na posição 3 pois o sort() colocou em forma crescente
+
+let pos2 = num.indexOf(4)
+
+if(pos2 == -1) { // Caso não tenha 4 na array...
+    console.log('O valor não foi encontrado')
+} else { // Caso tenha, irá mostrar
+    console.log(`O valor está na posição ${pos2}`)
+}
