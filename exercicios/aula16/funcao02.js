a1c689973bb0ac9cf172963b711172648f6f10f6
Joaquim <joaquimlima0612@gmail.com>
2025-07-01 15:50:37 -0300

    Funções

---
+++ exercicios/aula16/funcao02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0) { // Caso eu não preencha o n1 e/ou o n2, será considerado 0
+    return(n1 + n2)
+}
+
+console.log(soma(2,5)) // Irá retornar a soma entre 2 e 5
---
+++ exercicios/aula16/funcao03.js
@@ -0,0 +1,5 @@
+let v = function(x) {
+    return(x*2)
+}
+
+console.log(v(5)) // Passei para a minha variável um parâmetro e ele executou uma função
